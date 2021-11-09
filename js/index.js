ff136cf87e5451840710c228b33cd760b8a63397
aram5130 <starhigu@gmail.com>
2021-11-09 19:08:56 +0900

    2pge JS

--- js/index.js
+++ js/index.js
@@ -2,7 +2,28 @@ const popupBox = document.getElementById("popupButton");
 // console.log(popupBox);
 const popUp = document.getElementById("popUp");
 // console.log(popUp);
+const pics = document.getElementsByClassName("pic");
+//   console.log(pic);
+const lightbox = document.getElementById("lightbox");
+//   console.log(lightbox);
+const lightboxImage = document.getElementById("lightboxImage");
+//   console.log(lightboxImage);
 
 popupBox.addEventListener("click", () => {
     popUp.style.display = "block";
 })
+
+for (i = 0; i < pics.length; i++) {
+    pics[i].addEventListener("click", showLightbox);
+}
+function showLightbox() {
+    const bigLocation = this.getAttribute("data-src");
+    console.log(bigLocation);
+    lightboxImage.setAttribute("src", bigLocation);
+    lightbox.style.display = "block";
+}
+
+//   lightbox 닫기
+lightbox.onclick = function () {
+    lightbox.style.display = "none";
+};
