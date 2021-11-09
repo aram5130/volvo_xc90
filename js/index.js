79dc8a1efa893d4ddfdd44131cb4f50605532d9e
aram5130 <starhigu@gmail.com>
2021-11-09 18:40:02 +0900

    js start

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const popupBox = document.getElementById("popupButton");
+// console.log(popupBox);
+const popUp = document.getElementById("popUp");
+// console.log(popUp);
+
+popupBox.addEventListener("click", () => {
+    popUp.style.display = "block";
+})
