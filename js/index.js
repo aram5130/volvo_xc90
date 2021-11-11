24597a1c9b5282e06e0d3402de33cc91e64dc641
aram5130 <starhigu@gmail.com>
2021-11-11 17:03:09 +0900

    size 1020

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ const popupBox = document.getElementById("popupButton");
 // console.log(popupBox);
 const popUp = document.getElementById("popUp");
 // console.log(popUp);
+const popupCloseBtn = document.getElementById("popupClose");
+
+popupBox.addEventListener("click", () => {
+    popUp.style.display = "block";
+});
+popupCloseBtn.onclick = function() {
+    popUp.style.display = "none";
+};
+
+
 const pics = document.getElementsByClassName("pic");
 //   console.log(pic);
 const lightbox = document.getElementById("lightbox");
@@ -9,21 +19,17 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImage = document.getElementById("lightboxImage");
 //   console.log(lightboxImage);
 
-popupBox.addEventListener("click", () => {
-    popUp.style.display = "block";
-})
-
 for (i = 0; i < pics.length; i++) {
     pics[i].addEventListener("click", showLightbox);
 }
 function showLightbox() {
     const bigLocation = this.getAttribute("data-src");
-    console.log(bigLocation);
+    // console.log(bigLocation);
     lightboxImage.setAttribute("src", bigLocation);
     lightbox.style.display = "block";
 }
 
 //   lightbox 닫기
-lightbox.onclick = function () {
+lightbox.onclick = function() {
     lightbox.style.display = "none";
 };
